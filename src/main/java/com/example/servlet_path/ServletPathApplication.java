13af5cf0e16e9c1b6753ace14fab146a5cc05d60
Ryo Murai <murai.ry@gmail.com>
2025-05-13 18:21:26 +0900

    initial commit

---
+++ src/main/java/com/example/servlet_path/ServletPathApplication.java
@@ -0,0 +1,13 @@
+package com.example.servlet_path;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServletPathApplication.class, args);
+	}
+
+}
