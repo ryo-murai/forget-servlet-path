c27b5a28fb4bc335ac95875668976bcea6ae4d1f
Ryo Murai <murai.ry@gmail.com>
2025-05-13 18:21:31 +0900

    initial commit

---
+++ src/test/java/com/example/servlet_path/ServletPathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servlet_path;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletPathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
